2bc2e0b94e00c5a77793159fb50c496b9b1bc13e
jominmin <minwoobeanert@naver.com>
2024-09-02 12:15:47 +0900

    Create 1330.py

---
+++ 1330.py
@@ -0,0 +1,13 @@
+x, y = input().split()
+A = int(x)
+B = int(y)
+if A > B:
+    print('>')
+elif A < B:
+    print('<')
+elif A == B:
+    print('==')
+'''
+if () : 괄호 사용 안함
+if 다음부터는 elif 마지막은 else
+'''
