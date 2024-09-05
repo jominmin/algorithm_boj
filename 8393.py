cd5db7eeaf2ccd01dd9cc6d806539c4772b1b2e8
jominmin <minwoobeanert@naver.com>
2024-09-05 09:54:21 +0900

    Create 8393.py

---
+++ 8393.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+total = 0
+for i in range(1, n+1):
+    total += i
+print(total)
+
+'''
+n = int(input()
+print(sum(range(1, n+1)))
+'''
+
+'''
+n = int(input())
+for i in range(1,n+1):
+    i = i+i-1
+print(i)
+'''
