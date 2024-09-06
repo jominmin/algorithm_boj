e31c5c6cbc98bc69c73bb182d3337f9f514e26ef
jominmin <minwoobeanert@naver.com>
2024-09-06 09:01:36 +0900

    Create 15552.py

---
+++ 15552.py
@@ -0,0 +1,11 @@
+import sys #추가
+
+n = int(input())
+for i in range(n):
+    a, b = map(int,sys.stdin.readline().split()) 
+    print(a+b)
+
+'''
+input() 아니고 sys.stdin.readline()
+sys.stdin.readline() - 속도가 빠르다
+'''
