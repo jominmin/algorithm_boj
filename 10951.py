278d100a9b4e8ef4d0d6c016acd2d35625a90f7b
jominmin <minwoobeanert@naver.com>
2024-09-09 15:09:06 +0900

    Create 10951.py

---
+++ 10951.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        a,b = map(int,input().split())
+        print(a+b)
+    except:
+        break;
+
+'''
+try: 변수 A,B에 int형이 들어간다면, A+B의 값을 출력한다.
+except: try에 대한 에러가 발생한 경우(ex. a 1, 3, 2 ㄱ 입력)
+'''
