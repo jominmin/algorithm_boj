c152b19383d91b5f23c66c55ba532bdfc5e97362
jominmin <minwoobeanert@naver.com>
2024-09-09 15:09:00 +0900

    Create 10807.py

---
+++ 10807.py
@@ -0,0 +1,16 @@
+n = int(input())
+l = list(map(int,input().split())) # list
+v = int(input()) # 원하는 숫자
+
+print(l.count(v)) # list 중에 v가 얼마나 많은지
+
+'''
+a = int(input())
+d = int(input())
+t = 0 
+for i in range(a):
+    b = int(input())
+    if b == d:
+        t = t+1
+print(t)
+'''
