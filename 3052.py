83d3956a91fb63aeaaaa93e8ee5f89568afb9c00
jominmin <minwoobeanert@naver.com>
2024-09-18 10:47:50 +0900

    Create 3052.py

---
+++ 3052.py
@@ -0,0 +1,20 @@
+v = [] #빈 리스트 생성
+
+for i in range(10):
+    n = int(input())
+    if n%42 not in v: # 없으면
+        v.append(n%42) # 추가
+    '''
+    g = n%42
+    m = [g]
+    if m == n:
+        n.remove
+        '''
+print(len(v)) #길이
+    
+    
+
+
+
+
+
